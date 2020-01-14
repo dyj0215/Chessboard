544e99922a6f7225342039b07e3ed56c5f6bea46
dyj0215 <50331670+dyj0215@users.noreply.github.com>
2020-01-14 22:18:45 +0100

    Update 3_new.py

--- 3_new.py
+++ 3_new.py
@@ -1,13 +1,14 @@
 __author__ = 'Yijie Dong'
-
-import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from collections import defaultdict
+import numpy as np
 
-n = 8 #The dimension of the chessboard
-# chessboard = np.random.randint(0,2,(n,n))#Get a random array with only 0 and 1
-# chessboard[0][0] = chessboard[n-1][n-1] = 0#Both upper left corner and bottom right corner are available. equal to 0
-
-
+color1 = (0, 0, 1)
+color2 = (1, 1, 1)
+color3 = (0, 1, 0)
+n = 8
+# mat = np.zeros((8, 8))
 chessboard = [
     [0,0,0,0,0,1,1,0],
     [1,1,0,0,1,1,1,1],
@@ -18,70 +19,71 @@ chessboard = [
     [1,1,0,0,1,0,1,0],
     [0,1,0,0,1,0,0,0]
     ]
-print (chessboard)
+
+chessboard_with_path = np.reshape(chessboard, (64,1))
+
+
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('chessboard', [color1, color2], 2)
+cs = plt.imshow(chessboard, cmap=my_cmap)
+
+plt.xticks(np.linspace(0, 8, 8, endpoint=False), ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'), fontsize=20)
+plt.yticks(np.linspace(0, 8, 8, endpoint=False), ('1', '2', '3', '4', '5', '6', '7', '8'), fontsize=20)
+plt.tick_params(bottom=False, left=False, labeltop=True, labelright=True)
+plt.savefig("chessboard.png")
+plt.show()
+
 
 graph = defaultdict(list)
 
+for i in range(0, n):
+    for j in range(0, n - 1):
+        if chessboard[i][j] == chessboard[i][j + 1] == 0:
+            graph[int(n * i + j + 1)].append(int(n * i + j + 2))
+            graph[int(n * i + j + 2)].append(int(n * i + j + 1))
 
-for i in range(0,n):
-    for j in range(0,n-1):
-        if chessboard[i][j]==chessboard[i][j+1]==0:
-            graph[int(n*i+j+1)].append(int(n*i+j+2))
-            graph[int(n*i+j+2)].append(int(n*i+j+1))
-
-for j in range(0,n):
-    for i in range(0,n-1):
-        if chessboard[i][j]==chessboard[i+1][j]==0:
-            graph[int(n*i+j+1)].append(int(n*i+j+1+n))
-            graph[int(n*i+j+1+n)].append(int(n*i+j+1))
-
+for j in range(0, n):
+    for i in range(0, n - 1):
+        if chessboard[i][j] == chessboard[i + 1][j] == 0:
+            graph[int(n * i + j + 1)].append(int(n * i + j + 1 + n))
+            graph[int(n * i + j + 1 + n)].append(int(n * i + j + 1))
 
 
 # print(graph)
 
- 
 # Find the shortest path
-def findShortestPath(graph,start,end,path=[]):
-    path = path +[start]
+def findShortestPath(graph, start, end, path=[]):
+    path = path + [start]
     if start == end:
         return path
-    
+
     shortestPath = []
     for node in graph[start]:
         if node not in path:
-            newpath = findShortestPath(graph,node,end,path)
+            newpath = findShortestPath(graph, node, end, path)
             if newpath:
-                if not shortestPath or len(newpath)<len(shortestPath):
+                if not shortestPath or len(newpath) < len(shortestPath):
                     shortestPath = newpath
     return shortestPath
- 
-
- 
-onepath = findPath(graph,1,n*n)
-print('\none of all paths:',onepath)
- 
-allpath = findAllPath(graph,1,n*n)
-print('\nall the paths：',allpath)
- 
-shortpath = findShortestPath(graph,1,n*n)#I want to find the shortest path from upper left point to bottom right point. So i set 1 and n*n
-print('\nthe shortest path：',shortpath)
-print('\nsteps:', len(shortpath)-1)
 
 
+shortpath = findShortestPath(graph, 1, n * n)  # I want to find the shortest path from upper left point to bottom right point. So i set 1 and n*n
+
+for k in range(len(shortpath)):
+    p = shortpath[k]
+    chessboard_with_path[p-1] = 2
+
+chessboard_with_path = np.reshape(chessboard_with_path, (8,8))
+
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('chessboard_with_path', [color1, color2, color3], 3)
+cs = plt.imshow(chessboard_with_path, cmap=my_cmap)
+
+plt.xticks(np.linspace(0, 8, 8, endpoint=False), ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'), fontsize=20)
+plt.yticks(np.linspace(0, 8, 8, endpoint=False), ('1', '2', '3', '4', '5', '6', '7', '8'), fontsize=20)
+plt.tick_params(bottom=False, left=False, labeltop=True, labelright=True)
+plt.savefig("chessboard_with_path.png")
+plt.show()
 
 
-# chessboard = [
-#     [0,0,0,0,0,1,1,0],
-#     [1,1,0,0,1,1,1,1],
-#     [0,0,0,1,1,1,1,0],
-#     [0,1,1,0,0,0,0,1],
-#     [0,0,0,0,0,1,0,1],
-#     [1,1,0,1,1,0,0,1],
-#     [1,1,0,0,1,0,1,0],
-#     [0,1,0,0,1,0,0,0]
-#     ]
+print('\nthe shortest path：', shortpath)
 
-
-# the shortest path： [1, 2, 3, 11, 19, 18, 17, 25, 33, 34, 35, 36, 37, 29, 30, 31, 39, 47, 46, 54, 62, 63, 64]
-
-# steps: 22
+print('\nsteps:', len(shortpath) - 1)
